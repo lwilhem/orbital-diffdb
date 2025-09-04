4cb24dfbc67fd046a10f652d1aecd2379b80a61d
lwilhem <wilhem.lecanu@edu.devinci.fr>
2025-09-04 12:22:19 +0200

    chore: initial commit

---
+++ app/app.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+
+onMounted(() => {
+  colorMode.value = 'light'
+})
+</script>
+
+<template>
+  <div class="relative w-full h-96">
+    <BlackHoleBackground class="absolute inset-0 flex justify-center items-center rounded-xl" />
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,65 @@
+import tailwindcss from '@tailwindcss/vite'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@vueuse/nuxt',
+    '@nuxt/icon',
+    'reka-ui/nuxt',
+    'motion-v/nuxt',
+    '@nuxtjs/color-mode',
+    '@nuxt/eslint',
+  ],
+
+  devtools: {
+    enabled: true,
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  colorMode: {
+    classSuffix: '',
+  },
+
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  experimental: {
+    // when using generate, payload js assets included in sw precache manifest
+    // but missing on offline, disabling extraction it until fixed
+    payloadExtraction: false,
+    renderJsonPayloads: true,
+    typedPages: true,
+  },
+
+  compatibilityDate: '2025-07-15',
+
+  nitro: {
+    esbuild: {
+      options: {
+        target: 'esnext',
+      },
+    },
+  },
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  eslint: {
+    config: {
+      standalone: false,
+      nuxt: {
+        sortConfigKeys: true,
+      },
+    },
+  },
+
+  icon: {
+    mode: 'css',
+    cssLayer: 'base',
+  },
+})
